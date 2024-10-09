125f30ee79ab243ece33e2017dd7ded6b6207916
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-09 11:25:26 -0400

    did the code

--- main.ts
+++ main.ts
@@ -1,8 +1,39 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Pablo
+ * Created on: Sep 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+
+// variables
+let score: number = 0
+let randomNumber: number = 0 
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // if randomNumber was 0
+    if (randomNumber == 0) {
+    basic.showIcon(IconNames.SmallSquare)
+    }
+
+    // if randomNumber was 1
+    if (randomNumber == 1) {
+    basic.showIcon(IconNames.Square)
+    }
+
+    // if randomNumber was 2
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }})
+
+//score 
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.)
+})
