30fa791005ec549ebcd59dc41b916dd7397dfbcd
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-10 11:07:57 -0400

    change pause time

--- main.ts
+++ main.ts
@@ -21,21 +21,21 @@ input.onGesture(Gesture.Shake, function () {
     // if randomNumber was 0 is rock
     if (randomNumber == 0) {
         basic.showIcon(IconNames.SmallSquare)
-        basic.pause(3000)
+        basic.pause(2000)
         basic.showIcon(IconNames.Happy)
     }
 
     // if randomNumber was 1 is paper
     if (randomNumber == 1) {
         basic.showIcon(IconNames.Square)
-        basic.pause(3000)
+        basic.pause(2000)
         basic.showIcon(IconNames.Happy)
     }
 
     // if randomNumber was 2 is scossors 
     if (randomNumber == 2) {
         basic.showIcon(IconNames.Scissors)
-        basic.pause(3000)
+        basic.pause(2000)
         basic.showIcon(IconNames.Happy)
     }
 })
@@ -45,13 +45,13 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.showIcon(IconNames.Yes)
     score = (score + 1)
-    basic.pause(3000)
+    basic.pause(2000)
     basic.showIcon(IconNames.Happy)
 })
 
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString(" Score : + Score ")
-    basic.pause(3000)
+    basic.pause(2000)
     basic.showIcon(IconNames.Happy)
 })
